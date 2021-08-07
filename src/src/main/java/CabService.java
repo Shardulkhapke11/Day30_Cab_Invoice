ffae758e577633e099bb4d692fb5275d3ec68946
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-08-07 23:29:01 +0530

    All UC

---
+++ src/src/main/java/CabService.java
@@ -0,0 +1,27 @@
+package src.main.java;
+
+import java.util.List;
+
+public class CabService {
+    private static final double COST_PER_KM = 10 ;
+    private static final int COST_PER_MIN =1 ;
+    private static int  MIN_FARE= 5;
+
+    public double invoiceGenerator(double distance, int time) {
+        double totalFare = 0.0;
+        totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+        if( totalFare < MIN_FARE){
+            totalFare=5;
+        }
+        return totalFare ;
+    }
+
+    public InvoiceSummary invoiceGenerator(List<Ride> rides) {
+        double totalFare=0;
+        for (Ride ride : rides){
+            totalFare += this.invoiceGenerator(ride.distance,ride.time);
+            }
+//       double totalFare =(double) rides.stream().map(this :: invoiceGenerator);
+        return new InvoiceSummary(rides.size(), totalFare);
+    }
+}
---
+++ src/src/main/java/Ride.java
@@ -0,0 +1,13 @@
+package src.main.java;
+
+public class Ride {
+
+    public int time;
+    public double distance;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+
+    }
+}
